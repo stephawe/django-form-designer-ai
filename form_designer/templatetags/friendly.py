5c070dc392f456c9b42eac5744515066eee4d718
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-05-19 10:38:55 +0300

    Simplify the friendly tag

--- form_designer/templatetags/friendly.py
+++ form_designer/templatetags/friendly.py
@@ -2,7 +2,6 @@ from django import template
 from django.db.models.query import QuerySet
 from django.template.defaultfilters import yesno
 from django.utils.encoding import force_text
-from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
@@ -12,12 +11,10 @@ register = template.Library()
 def friendly(value, null_value=None):
     if value is None and not (null_value is None):
         return null_value
-    if isinstance(value, QuerySet):
-        value = [force_text(object) for object in value]
-    if isinstance(value, list):
-        value = ", ".join(value)
+    if isinstance(value, (QuerySet, list)):
+        value = ", ".join(force_text(object) for object in value)
     if isinstance(value, bool):
-        value = yesno(value, u"%s,%s" % (_('yes'), _('no')),)
+        value = yesno(value)
     if hasattr(value, 'url'):
         value = value.url
     return force_text(value)
