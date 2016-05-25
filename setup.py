25f17075323173fb60c37e32441c5f5a3165203b
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-05-25 15:09:05 +0300

    Bump to 0.9.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-form-designer-ai',
-    version='0.9.0',
+    version='0.9.1',
     url='http://github.com/andersinno/django-form-designer-ai',
     license='BSD',
     maintainer='Anders Innovations Ltd',
